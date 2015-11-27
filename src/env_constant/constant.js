54b13ccb7ec0d8657248ee6e06419ee3837eddc5
triadiprabowo <triadiprabowo@gmail.com>
2015-11-27 08:21:39 +0700

    Add environment file and constant

---
+++ src/env_constant/constant.js
@@ -0,0 +1,13 @@
+/* 
+** Constant seed 1.0
+** triadiprabowo.xyz
+** Developed by Triadi Prabowo
+*/
+
+module.exports = (function() {
+	'use strict'
+
+	return {
+		__1: 'Define your constant here! You can also set function in this seed'
+	}
+})();
